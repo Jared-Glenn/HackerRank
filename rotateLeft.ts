02c455f8e9cd59e183e5aa00aa98d28ca12b6164
Jared <jared_glenn@yahoo.com>
2024-09-07 23:55:53 -0600

    fixed issue with reassigning in place

--- rotateLeft.ts
+++ rotateLeft.ts
@@ -1,22 +1,16 @@
 function reverse(arr: number[], start: number, end: number): number[] {
     while (start < end) {
-        [arr[start], arr[end] = arr[end], arr[start]];
+        [arr[start], arr[end]] = [arr[end], arr[start]];
         start++;
         end--;
-        console.log("Reverse:", arr);
     }
     return arr;
 }
 
 function rotLeft(a: number[], d: number): number[] {
     reverse(a, 0, (a.length - 1));
-    console.log(a);
-    reverse(a, 0, (a.length-d));
-    console.log(a.length-d);
-    console.log(a[a.length-d]);
-    reverse(a, (a.length-d+1), (a.length-1));
-    console.log(a.length-d+1);
-    console.log(a[a.length-d+1]);
+    reverse(a, 0, (a.length-d-1));
+    reverse(a, (a.length-d), (a.length-1));
 
     return a;
 }
